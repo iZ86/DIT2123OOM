95a2de74391dbd34ee211797ab012a45256b8dfd
iZ86 <iZLikesCS@gmail.com>
2024-05-05 15:04:20 +0800

    Added comments

--- BoardingPassView.java
+++ BoardingPassView.java
@@ -1,39 +1,58 @@
 import javax.swing.*;
 import java.awt.event.ActionListener;
 
+/** This class represents the view to show the boarding pass. */
 public class BoardingPassView {
+    /** JPanel for BoardingPassView. */
     private JPanel boardingPassViewPanel;
+    /** The model that holds the passenger and their bag data. */
     private KioskCheckInModel kioskCheckInModel;
-    private JButton previousBoardingPassButton = new JButton("p revious");
+    /** JButton to show the previous passenger's and their bag's data,
+     * this button won't be displayed if the passenger is the first passenger on the list.
+     */
+    private JButton previousBoardingPassButton = new JButton("previous");
+    /** JButton to show the next passenger's and their bag's data,
+     * this button won't be displayed if the passenger is the last passenger on the list.
+     */
     private JButton nextBoardingPassButton = new JButton("next");
+    /** JButton that saves a PDF file of the BoardingPassView. */
     private JButton printBoardingPassButton = new JButton("print");
+    /** JButton that changes the view to the MainMenuView. */
     private JButton mainMenuButton = new JButton("Main Menu");
 
-    public BoardingPassView(KioskCheckInModel kioskCheckInModel){
-        setUpViewPanel();
+    /** Creates a BoardingPassView object with KioskCheckInModel kioskCheckInModel as the model. */
+    public BoardingPassView(KioskCheckInModel kioskCheckInModel) {
+        this.kioskCheckInModel = kioskCheckInModel;
+        setupViewPanel();
     }
 
-    private void setUpViewPanel(){
+    /** Sets up the boardingPassViewPanel. */
+    private void setupViewPanel() {
         boardingPassViewPanel = new JPanel();
     }
 
-    public JPanel getBoardingPassViewPanel(){
+    /** Returns the boardingPassViewPanel. */
+    public JPanel getBoardingPassViewPanel() {
         return boardingPassViewPanel;
     }
 
-    public void addPreviousBoardingPassButtonListener(ActionListener listenForPreviousBoardingPassButton){
+    /** Adds an ActionListener to previousBoardingPassButton. */
+    public void addPreviousBoardingPassButtonListener(ActionListener listenForPreviousBoardingPassButton) {
         previousBoardingPassButton.addActionListener(listenForPreviousBoardingPassButton);
     }
 
-    public void addNextBoardingPassButtonListener(ActionListener listenForNextBoardingPassButton){
+    /** Adds an ActionListener to nextBoardingPassButton. */
+    public void addNextBoardingPassButtonListener(ActionListener listenForNextBoardingPassButton) {
         nextBoardingPassButton.addActionListener(listenForNextBoardingPassButton);
     }
 
-    public void addPrintBoardingPassButtonListener(ActionListener listenForPrintBoardingPassButton){
+    /** Adds an ActionListener to printBoardingPassButton. */
+    public void addPrintBoardingPassButtonListener(ActionListener listenForPrintBoardingPassButton) {
         printBoardingPassButton.addActionListener(listenForPrintBoardingPassButton);
     }
 
-    public void addMainMenuButtonListener(ActionListener listenForMainMenuButton){
+    /** Adds an ActionListener to mainMenuButton. */
+    public void addMainMenuButtonListener(ActionListener listenForMainMenuButton) {
         mainMenuButton.addActionListener(listenForMainMenuButton);
     }
 }
