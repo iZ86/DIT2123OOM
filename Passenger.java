bd236a5dedbef7d796ac812311704b74c43d49d0
iZ86 <iZLikesCS@gmail.com>
2024-05-08 20:38:15 +0800

    Added specialNeed variable and getter and setter method for it

--- Passenger.java
+++ Passenger.java
@@ -1,14 +1,16 @@
 public class Passenger {
-    /**Passport number of the passenger*/
+    /** Passport number of the passenger. */
     private int passportNumber; 
-    /**bookingID of the passenger*/
+    /** bookingID of the passenger. */
     private String bookingID; 
-    /**Full name of the passenger */
+    /** Full name of the passenger. */
     private String fullName; 
-    /**Bags of the passenger */
-    private Bag[] bags; 
+    /** Bags of the passenger. */
+    private Bag[] bags;
+    /** Special needs status of the passenger. */
+    private boolean specialNeed;
 
-    /**Creates a Passenger object to hold data */
+    /** Creates a Passenger object to hold data. */
     public Passenger(String bookingID, int passportNumber, String fullName, Bag[] bags) {
         this.bookingID = bookingID;
         this.passportNumber = passportNumber;
@@ -16,12 +18,12 @@ public class Passenger {
         this.bags = bags;
     }
 
-    /**Returns the bookingID */
+    /** Returns the bookingID. */
     public String getBookingID() {
         return bookingID;
     }
 
-    /**Returns the passport number */
+    /** Returns the passport number. */
     public int getPassportNumber() {
         return passportNumber;
     }
@@ -31,8 +33,18 @@ public class Passenger {
         return fullName;
     }
 
-    /**Returns the bag at int INDEX, returns null, if the bag index doesn't exist */
+    /** Returns the bag at int INDEX, returns null, if the bag index doesn't exist. */
     public Bag getBag(int index) {
         return bags[index];
+    }
+
+    /** Returns specialNeed. */
+    public boolean isSpecialNeed() {
+        return specialNeed;
+    }
+
+    /** Sets specialNeed. */
+    public void setSpecialNeed(boolean specialNeed) {
+        this.specialNeed = specialNeed;
     }
 }
