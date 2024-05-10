6d1447421df9900f3fb5533404d916b72ac87220
iZ86 <iZLikesCS@gmail.com>
2024-05-10 22:02:50 +0800

    Starter code for Utils

--- Utils.java
+++ Utils.java
@@ -1,7 +1,15 @@
+import java.util.Random;
 
 /** This class represents the utilities for this project,
  *  it will be used for getting random information.
  */
 public class Utils {
+    /** A constant array of destinations that an airport can reach. */
+    public static final String[] DESTINATIONS = new String[]{};
+
+
+    /** random object used to decide which data should be used. */
+    private Random random;
+
 
 }
