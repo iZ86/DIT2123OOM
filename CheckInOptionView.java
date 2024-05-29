1ce7617cf0e84915dd16ac069c815dcdddddbd40
iZ86 <iZLikesCS@gmail.com>
2024-05-29 11:17:52 +0800

    Modified CheckInOptionViewPanel variable to checkInOptionViewPanel, put the titleLabel into setupViewPanel method, added new warnMinimumNumberOfPassengers boolean attribute, modified setupViewPanel name, modified so that their positioning uses the right methods in setupViewPanel and implemented the error label for minimum number of passengers, added resetView, clearView, updateView and setWarnMinimumNumberOfPassengers methods

--- CheckInOptionView.java
+++ CheckInOptionView.java
@@ -5,8 +5,8 @@ import java.awt.event.*;
 /* This class represents the view for choosing the check in options, this allows the user to choose between single and group check in. */
 public class CheckInOptionView {
     
-    /** JPanel for the CheckInOptionView */
-    private JPanel CheckInOptionViewPanel = new JPanel(new GridBagLayout());
+    /** JPanel for the checkInOptionView */
+    private JPanel checkInOptionViewPanel = new JPanel(new GridBagLayout());
 
     /** JButton that changes the view to the MainMenuView */
     private JButton mainMenuButton = new JButton("Main Menu");
@@ -19,74 +19,116 @@ public class CheckInOptionView {
 
     /** JSpinner to select the number of passengers */
     private JSpinner numberOfPassengers = new JSpinner();
+    /** True if there should be warning prompt for minimum number of passengers. */
+    private boolean warnMinimumNumberOfPassengers;
 
-    /* Title */
-    JLabel titleText = new JLabel("Check In Option");
 
     /** Creates a CheckInOptionView object */
     public CheckInOptionView(){
-        setupCheckInOptionViewPanel();
+        setupViewPanel();
     }
 
     /** Sets up the CheckInOptionViewPanel */
-    private void setupCheckInOptionViewPanel(){
+    private void setupViewPanel(){
+
+
+        JLabel titleText = new JLabel("Check In Option");
+
         GridBagConstraints mainMenuButtonConstraints = new GridBagConstraints();
         GridBagConstraints titleTextConstraints = new GridBagConstraints();
         GridBagConstraints groupButtonConstraints = new GridBagConstraints();
         GridBagConstraints singleButtonConstraints = new GridBagConstraints();
         GridBagConstraints numberOfPeopleConstraints = new GridBagConstraints();
 
-        // Set size for all components
-        mainMenuButton.setPreferredSize(new Dimension(100, 40));
-        groupButton.setPreferredSize(new Dimension(100, 40));
-        singleButton.setPreferredSize(new Dimension(100, 40));
-        numberOfPassengers.setPreferredSize(new Dimension(100, 40));
+        int widthSizeOfButton = 20;
+        int heightSizeOfButton = 10;
+        int widthOfJSpanner = 65;
+        int heightOfJSpanner = 15;
 
         // Title
         titleText.setFont(new Font("Arial", Font.BOLD, 17));
-        titleTextConstraints.ipadx = 0;
-        titleTextConstraints.ipady = 0;
+        titleTextConstraints.ipadx = widthSizeOfButton;
+        titleTextConstraints.ipady = heightSizeOfButton;
         titleTextConstraints.gridx = 0;
         titleTextConstraints.gridy = 0;
-        titleTextConstraints.insets = new Insets(0, 0, 300, 0);
-        CheckInOptionViewPanel.add(titleText, titleTextConstraints);
-
-        // Add Back To Main Menu button to panel
-        mainMenuButtonConstraints.ipadx = 0;
-        mainMenuButtonConstraints.ipady = 0;
-        mainMenuButtonConstraints.gridx = 0;
-        mainMenuButtonConstraints.gridy = 0;
-        mainMenuButtonConstraints.insets = new Insets(300, 0, 0, 0);
-        CheckInOptionViewPanel.add(mainMenuButton, mainMenuButtonConstraints);
-
-        // Add Single button to panel
-        singleButtonConstraints.ipadx = 0;
-        singleButtonConstraints.ipady = 0;
-        singleButtonConstraints.gridx = 0;
-        singleButtonConstraints.gridy = 0;
-        singleButtonConstraints.insets = new Insets(0, 0, 50, 175);
-        CheckInOptionViewPanel.add(singleButton, singleButtonConstraints);
-        
-        // Add Group button to panel
-        groupButtonConstraints.ipadx = 0;
-        groupButtonConstraints.ipady = 0;
-        groupButtonConstraints.gridx = 0;
-        groupButtonConstraints.gridy = 0;
-        groupButtonConstraints.insets = new Insets(0, 175, 50, 0);
-        CheckInOptionViewPanel.add(groupButton, groupButtonConstraints);
+        titleTextConstraints.gridwidth = 2;
+        titleTextConstraints.insets = new Insets(0, 0, 0, 0);
+        checkInOptionViewPanel.add(titleText, titleTextConstraints);
 
         // JSpinner for number of people
-        numberOfPeopleConstraints.ipadx = 0;
-        numberOfPeopleConstraints.ipady = 0;
-        numberOfPeopleConstraints.gridx = 0;
-        numberOfPeopleConstraints.gridy = 0;
-        numberOfPeopleConstraints.insets = new Insets(0 ,175, 150, 0);
-        CheckInOptionViewPanel.add(numberOfPassengers, numberOfPeopleConstraints);
+        numberOfPeopleConstraints.ipadx = widthOfJSpanner;
+        numberOfPeopleConstraints.ipady = heightOfJSpanner;
+        numberOfPeopleConstraints.gridx = 1;
+        numberOfPeopleConstraints.gridy = 1;
+        // Insets 15 to the left to follow the group button.
+        numberOfPeopleConstraints.insets = new Insets(40 ,15, 0, 0);
+        checkInOptionViewPanel.add(numberOfPassengers, numberOfPeopleConstraints);
+
+        // Add Single button to panel
+        singleButtonConstraints.ipadx = widthSizeOfButton;
+        singleButtonConstraints.ipady = heightSizeOfButton;
+        singleButtonConstraints.gridx = 0;
+        singleButtonConstraints.gridy = 2;
+        singleButtonConstraints.insets = new Insets(10, 0, 0, 15);
+        checkInOptionViewPanel.add(singleButton, singleButtonConstraints);
+        
+        // Add Group button to panel
+        groupButtonConstraints.ipadx = widthSizeOfButton;
+        groupButtonConstraints.ipady = heightSizeOfButton;
+        groupButtonConstraints.gridx = 1;
+        groupButtonConstraints.gridy = 2;
+        groupButtonConstraints.insets = new Insets(10, 15, 0, 0);
+        checkInOptionViewPanel.add(groupButton, groupButtonConstraints);
+
+        if (warnMinimumNumberOfPassengers) {
+            JLabel warnMinimumNumberOfPassengersLabel = new JLabel("Minimum number is 2!");
+            warnMinimumNumberOfPassengersLabel.setFont(new Font("Arial", Font.PLAIN, 8));
+
+            warnMinimumNumberOfPassengersLabel.setForeground(Color.RED);
+            GridBagConstraints constraintsForWarnMinimumNumberOfPassengersLabel = new GridBagConstraints();
+
+            constraintsForWarnMinimumNumberOfPassengersLabel.gridx = 1;
+            constraintsForWarnMinimumNumberOfPassengersLabel.gridy = 3;
+            constraintsForWarnMinimumNumberOfPassengersLabel.insets = new Insets(0, 15, 0, 0);
+            checkInOptionViewPanel.add(warnMinimumNumberOfPassengersLabel, constraintsForWarnMinimumNumberOfPassengersLabel);
+        }
+
+        // Add Back To Main Menu button to panel
+        mainMenuButtonConstraints.ipadx = widthSizeOfButton;
+        mainMenuButtonConstraints.ipady = heightSizeOfButton;
+        mainMenuButtonConstraints.gridx = 0;
+        mainMenuButtonConstraints.gridy = 4;
+        mainMenuButtonConstraints.gridwidth = 2;
+        mainMenuButtonConstraints.insets = new Insets(50, 0, 0, 0);
+        checkInOptionViewPanel.add(mainMenuButton, mainMenuButtonConstraints);
     }
 
+    /** Resets the view from all user inputs and removes all warnings. */
+    public void resetView() {
+        setNumberOfPassengers(2);
+        setWarnMinimumNumberOfPassengers(false);
+    }
+
+    /** Clears the view. */
+    private void clearView() {
+        checkInOptionViewPanel.removeAll();
+    }
+
+    /** Updates the view. */
+    public void updateView() {
+        clearView();
+        setupViewPanel();
+        checkInOptionViewPanel.revalidate();
+        checkInOptionViewPanel.repaint();
+    }
+
+    /** Sets warnMinimumNumberOfPassengers. */
+    public void setWarnMinimumNumberOfPassengers(boolean warnMinimumNumberOfPassengers) {
+        this.warnMinimumNumberOfPassengers = warnMinimumNumberOfPassengers;
+    }
     /** Returns the CheckInOptionViewPanel */
     public JPanel getCheckInOptionViewPanel(){
-        return CheckInOptionViewPanel;
+        return checkInOptionViewPanel;
     }
 
     /** Returns the number of passengers in JSpinners numberOfPassenger. */
