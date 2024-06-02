5e42fe568c1f67218e3438b772c036ed4852f1da
iZ86 <iZLikesCS@gmail.com>
2024-06-02 16:45:55 +0800

    Removed all the staff information variables and added staff object variable, made constructor initialize staff data, modified all getter methods to use staff object, modified nextCounter method

--- CounterModel.java
+++ CounterModel.java
@@ -2,47 +2,40 @@
 /** This class is the model for the CounterView class. */
 public class CounterModel {
 
-    /** The designated counter number for the user. */
-    private int counterNumber;
-    /** The name of staff at the designated counter. */
-    private String staffName;
-    /** The role of the staff at the designated counter. */
-    private String staffRole;
-    /** The age of the staff at the designated counter. */
-    private int staffID;
+    /** Staff object that holds all the staff data. */
+    private Staff staff;
     /** Utils for getting random information. */
     private Utils utils;
 
     public CounterModel() {
         utils = new Utils();
+        // Initialization
+        nextCounter();
     }
 
     /** Returns counterNumber. */
-    public int getCounterNumber() {
-        return counterNumber;
+    public int getCounter() {
+        return staff.getCounter();
     }
 
     /** Returns staffName. */
     public String getStaffName() {
-        return staffName;
+        return staff.getStaffName();
     }
 
     /** Returns staffRole. */
     public String getStaffRole() {
-        return staffRole;
+        return staff.getStaffRole();
     }
 
-    /** Returns staffAge. */
-    public int getStaffID() {
-        return staffID;
+    /** Returns staffID. */
+    public String getStaffID() {
+        return staff.getStaffID();
     }
 
     /** Gets a new Counter and sets its data in the model. */
     public void nextCounter() {
-        counterNumber = 0;
-        staffName = "Staff1";
-        staffRole = "Role1";
-        staffID = 1;
+        staff = utils.getRandomStaff();
     }
 
 }
