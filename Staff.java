deb0a3ce455518152fa7a5520a043ee57b3d6053
iZ86 <iZLikesCS@gmail.com>
2024-06-02 16:41:51 +0800

    Added comments

--- Staff.java
+++ Staff.java
@@ -1,9 +1,16 @@
+
+/** This class represents the data for the aerocheck in staff. */
 public class Staff {
+    /** Staff id. */
     private String staffID;
+    /** Staff name. */
     private String staffName;
+    /** Staff role. */
     private String staffRole;
+    /** Counter. */
     private int counter;
 
+    /** Creates a new Staff object for holding staff data. */
     public Staff(String staffID, String staffName, String staffRole, int counter) {
         this.staffID = staffID;
         this.staffName = staffName;
@@ -11,18 +18,22 @@ public class Staff {
         this.counter = counter;
     }
 
+    /** Returns staffID. */
     public String getStaffID() {
         return staffID;
     }
 
+    /** Returns staffName. */
     public String getStaffName() {
         return staffName;
     }
 
+    /** Returns staffRole. */
     public String getStaffRole() {
         return staffRole;
     }
 
+    /** Returns counter. */
     public int getCounter() {
         return counter;
     }
