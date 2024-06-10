93eee7a4973f3a23a971cea7aedc634afd53d28f
iZ86 <iZLikesCS@gmail.com>
2024-06-10 17:15:09 +0800

    Added special accommodation status for the passenger, removed Bag as a parameter from the constructor and set it to null

--- Passenger.java
+++ Passenger.java
@@ -9,18 +9,33 @@ public class Passenger {
     private Bag[] bags;
     /** Special needs status of the passenger. */
     private boolean specialNeed;
+    /** Wheelchair status of the passenger. */
+    private boolean requireWheelchair;
+    /** Blindness status of the passenger. */
+    private boolean blind;
+    /** Deafness status of the passenger. */
+    private boolean deaf;
+    /** Other Special Accommodation status of the passenger. */
+    private boolean otherSpecialAccommodation;
+    /** Special Accommodation details of the passenger. */
+    private String otherSpecialAccommodationDetails;
 
     /** Creates a Passenger object to hold data. */
-    public Passenger(String bookingID, String passportNumber, String fullName, Bag[] bags) {
+    public Passenger(String bookingID, String passportNumber, String fullName) {
         this.bookingID = bookingID;
         this.passportNumber = passportNumber;
         this.fullName = fullName;
-        this.bags = bags;
+        bags = null;
     }
 
     /** Returns the number of bags the passenger has. */
     public int getNumberOfBags() {
         return bags.length;
+    }
+
+    /** Sets the bags. */
+    public void setBags(Bag[] bags) {
+        this.bags = bags;
     }
 
     /** Returns the bookingID. */
@@ -52,4 +67,55 @@ public class Passenger {
     public void setSpecialNeed(boolean specialNeed) {
         this.specialNeed = specialNeed;
     }
+
+    /** Returns requireWheelchair. */
+    public boolean isRequireWheelchair() {
+        return requireWheelchair;
+    }
+
+    /** Sets requireWheelchair. */
+    public void setRequireWheelchair(boolean requireWheelchair) {
+        this.requireWheelchair = requireWheelchair;
+    }
+
+    /** Returns blind. */
+    public boolean isBlind() {
+        return blind;
+    }
+
+    /** Sets blind. */
+    public void setBlind(boolean blind) {
+        this.blind = blind;
+    }
+
+    /** Returns deaf. */
+    public boolean isDeaf() {
+        return deaf;
+    }
+
+    /** Sets deaf. */
+    public void setDeaf(boolean deaf) {
+        this.deaf = deaf;
+    }
+
+    /** Returns otherSpecialAccommodation. */
+    public boolean isOtherSpecialAccommodation() {
+        return otherSpecialAccommodation;
+    }
+
+    /** Sets otherSpecialAccommodation. */
+    public void setOtherSpecialAccommodation(boolean otherSpecialAccommodation) {
+        this.otherSpecialAccommodation = otherSpecialAccommodation;
+    }
+
+    /** Return otherSpecialAccommodationDetails. */
+    public String getOtherSpecialAccommodationDetails() {
+        return otherSpecialAccommodationDetails;
+    }
+
+    /** Sets otherSpecialAccommodationDetails. */
+    public void setOtherSpecialAccommodationDetails(String otherSpecialAccommodationDetails) {
+        this.otherSpecialAccommodationDetails = otherSpecialAccommodationDetails;
+    }
+
 }
