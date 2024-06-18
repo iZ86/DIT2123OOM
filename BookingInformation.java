02acaf0e2fa84eef899241583c59bb6b44d0e963
iZ86 <iZLikesCS@gmail.com>
2024-06-18 09:34:36 +0800

    Added getter and setter methods for passportNumber and fullName, added comments

--- BookingInformation.java
+++ BookingInformation.java
@@ -1,16 +1,26 @@
+/** This class holds the booking information data. */
 public class BookingInformation {
-    String bookingID;
+    /** Booking Number. */
+    String bookingNumber;
+    /** Passport Number. */
     String passportNumber;
+    /** Full name. */
     String fullName;
+    /** Seat number. */
     String seatNumber;
+    /** Destination. */
     String destination;
+    /** Flight status. */
     String flightStatus;
+    /** Gate number. */
     int gateNumber;
+    /** Boarding time. */
     String boardingTime;
 
-    public BookingInformation(String bookingID, String passportNumber, String fullName, String seatNumber, String destination,
+    /** Creates a BookingInformation object with all the booking information data. */
+    public BookingInformation(String bookingNumber, String passportNumber, String fullName, String seatNumber, String destination,
                               String flightStatus, int gateNumber, String boardingTime) {
-        this.bookingID = bookingID;
+        this.bookingNumber = bookingNumber;
         this.passportNumber = passportNumber;
         this.fullName = fullName;
         this.seatNumber = seatNumber;
@@ -20,26 +30,42 @@ public class BookingInformation {
         this.boardingTime = boardingTime;
     }
 
-    public String getBookingID() {
-        return bookingID;
+    /** Returns bookingNumber. */
+    public String getBookingNumber() {
+        return bookingNumber;
     }
 
+    /** Returns passportNumber. */
+    public String getPassportNumber() {
+        return passportNumber;
+    }
+
+    /** Returns fullName. */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /** Returns seatNumber. */
     public String getSeatNumber() {
         return seatNumber;
     }
 
+    /** Returns destination. */
     public String getDestination() {
         return destination;
     }
-    
+
+    /** Returns flightStatus. */
     public String getFlightStatus() {
         return flightStatus;
     }
 
+    /** Returns gateNumber. */
     public int getGateNumber() {
         return gateNumber;
     }
 
+    /** Returns boardingTime. */
     public String getBoardingTime() {
         return boardingTime;
     }
