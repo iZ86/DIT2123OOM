fdd5d9ac20fa1288fe32ee884df8705c271ae5a6
iZ86 <iZLikesCS@gmail.com>
2024-06-23 15:52:36 +0800

    Removed comments

--- Main.java
+++ Main.java
@@ -1,13 +1,11 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Does not work currently.
         KioskCheckInModel kioskCheckInModel = new KioskCheckInModel();
         CounterModel counterModel = new CounterModel();
         GUI gui = new GUI(counterModel, kioskCheckInModel);
         Controller controller = new Controller(gui, counterModel, kioskCheckInModel);
 
-        // Does not work currently.
         gui.display();
     }
 
